1d464ebd4cb9ab9cd8f5459b860878e18eab78cb
zhaohongguang <zhaohongguang@yundianjia.com>
2018-01-30 15:28:27 +0800

    first commit

---
+++ lib/abutment_odoo.rb
@@ -0,0 +1,30 @@
+require "abutment_odoo/version"
+require "xmlrpc/client"
+
+module AbutmentOdoo
+
+  class << self
+
+    def initialize(options={})
+      @url           = options[:url]
+      @database_name = options[:database_name]
+      @password      = options[:password]
+      @username      = options[:username]
+    end
+
+    def udi(options = {})
+      common = XMLRPC::Client.new2("#{@url}/xmlrpc/2/common")
+      uid = common.call('authenticate', @database_name, @username, @password, {})
+    end
+
+    def search()
+      operate_models(model_name, 'search', options = {})
+    end
+
+    def operate_models(model_name, operate_type, options = {})
+      models = XMLRPC::Client.new2("#{url}/xmlrpc/2/object").proxy
+
+      models.execute_kw(@database_name, uid, @password, "#{model_name}", "#{operate_type}", , )
+    end
+  end
+end
---
+++ lib/abutment_odoo/version.rb
@@ -0,0 +1,3 @@
+module AbutmentOdoo
+  VERSION = "0.1.0"
+end
